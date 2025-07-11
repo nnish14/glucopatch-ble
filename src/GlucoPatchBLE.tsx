ce68f787092acc9671993c0e9d7772fbcf6d930d
N Nishchit <121371298+nnish14@users.noreply.github.com>
2025-07-11 14:40:11 +0530

    GlucoPatchBLE.tsx

--- src/GlucoPatchBLE.tsx
+++ src/GlucoPatchBLE.tsx
@@ -87,7 +87,6 @@ export default function GlucoPatchBLE() {
         <div style={{ marginTop: 20 }}>
           <h2>🧪 Glucose: {glucoseData.glucose_mg_dL} mg/dL</h2>
           <p>🌡️ Temperature: {glucoseData.temperature_C.toFixed(2)} °C</p>
-          <p>🕒 Timestamp: {glucoseData.timestamp}</p>
         </div>
       )}
 
